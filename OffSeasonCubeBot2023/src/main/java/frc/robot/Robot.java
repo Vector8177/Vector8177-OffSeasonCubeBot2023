8150999e3bf7bced00a88490360c4c91e6f35e23
ZaraSofia <112798040+ZaraSofia@users.noreply.github.com>
2023-11-01 15:29:04 -0500

    Added advantage kit as a repository
    Made slight changes to the build.gradle file that are required for advantage kit and imported the repository.

--- OffSeasonCubeBot2023/src/main/java/frc/robot/Robot.java
+++ OffSeasonCubeBot2023/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+
+
+import org.littletonrobotics.junction.LoggedRobot;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -14,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -25,6 +28,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
+    //Configures and initializes the logging framework. This has to be the first thing to be initialized in this method. 
+    Logger.recordMetadata("","")
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
